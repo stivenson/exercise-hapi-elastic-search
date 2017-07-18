43e055bbfdd2de86998c81b885e68448fedabcb5
stivenson <stivenson.rpm@gmail.com>
2017-07-17 22:12:18 -0500

    Basic Settings

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-var Hapi = require('hapi');
- 
-var server = new Hapi.Server();
- 
-server.pack.register({
-  plugin: require('elasticsearch-hapi-plugin'),
-  options: {
-    host: "http://localhost:9200"
-  }
+'use strict';
+const Hapi = require('hapi');
+const server = new Hapi.Server();
+server.connection({port: 3000});
+
+    
+
+server.register({
+    register: require('elasticsearch-hapi-plugin'),
+    host: 'http://localhost:9200'
 }, (err) => {
-  if (err) return;
- 
-  server.start();
+    if (err) return;
+    server.start();
 });
