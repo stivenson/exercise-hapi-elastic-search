632be29191330b99768118a72d20ad123291c7cd
stivenson <stivenson.rpm@gmail.com>
2017-07-18 01:10:41 -0500

    Basic examples tested

---
+++ README.md
@@ -0,0 +1,6 @@
+# exercise-hapi-elastic-search
+
+
+- Install and run elasticsearch and nodejs v8
+- Run `npm install`
+- Run `node .` 
--- index.js
+++ index.js
@@ -1,14 +1,72 @@
 'use strict';
 const Hapi = require('hapi');
 const server = new Hapi.Server();
+const elasticsearch = require('elasticsearch-hapi-plugin');
 server.connection({port: 3000});
 
+/**
+ * Register plugin
+ */
+server.register({
+    register: elasticsearch,
+    host: 'http://192.168.0.14:9200' // default ip may vary 
+}, (err) => {
+    
+    if (err) { 
+        console.log('Error connecting to Elasticsearch');
+        return;
+    };
+
+    server.start();
+
+
+    // Basic uses
+
+    //*  get reference to plugin */
+    let client = server.plugins['elasticsearch-hapi-plugin'].es;
     
 
-server.register({
-    register: require('elasticsearch-hapi-plugin'),
-    host: 'http://localhost:9200'
-}, (err) => {
-    if (err) return;
-    server.start();
+    console.log(client);
+
+
+    /**
+     * Ping 
+     */
+    client.ping({
+      requestTimeout: 30000,
+    }, (error) => {
+        if (error) {
+            console.error('Elasticsearch with hapi is down! :(');
+            console.log(error);
+        } else {
+            console.log('Elasticsearch with hapi is great! :)');
+        }
+    });
+
+    /**
+     *
+     */
+
+    client.search({
+      q: 'pants'
+    }).then(function (body) {
+      console.log(body.hits.hits);
+    }, function (error) {
+      console.trace(error.message);
+    });
+
+   /**
+    * In mac
+    * 1. Install elasticsearch with brew
+    * 2. Edit file in path: /usr/local/etc/elasticsearch/elasticsearch.yml (optional, for changes of ip and port , etc.)
+    * 3. Restart mac and after run elasticsearch (not background alternative)
+    */
+
+    /*
+     * Use nodejs v8
+     * and run command: node .
+     */
+
+    console.log('#### CTRL-C for exit ####');
+
 });
